6d58af8d4b5e957b21f8a844786db168ecf9c560
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2023-10-04 11:46:20 +0530

    05___nested-comment completed

---
+++ 05___nested-comment/index.js
@@ -0,0 +1,43 @@
+let commentContainer = document.getElementById("comment-container");
+
+function createInputBox() {
+  let div = document.createElement("div");
+  div.setAttribute("class", "comment-details");
+
+  div.innerHTML += `
+    <input placeholder="add text here" class="input" type="text">
+    <button class="btn submit">Submit</button>`;
+  return div;
+}
+
+function addReply(text) {
+  let div = document.createElement("div");
+  div.setAttribute("class", "all-comment");
+
+  div.innerHTML += `
+    <div class="card">
+    <span class="text">${text}</span>
+    <span class="reply" id="reply">Add Reply</span>
+    </div>
+    `;
+  return div;
+}
+
+commentContainer.addEventListener("click", function (e) {
+  let replyClicked = e.target.classList.contains("reply");
+  let submitClicked = e.target.classList.contains("submit");
+  let closestCard = e.target.closest(".all-comment");
+
+  if (replyClicked) {
+    closestCard.appendChild(createInputBox());
+  }
+  if (submitClicked) {
+    const commentDetails=e.target.closest(".comment-details");
+    if(commentDetails.children[0].value){
+        closestCard.appendChild(addReply(commentDetails.children[0].value));
+        commentDetails.remove();
+    }
+
+  }
+});
+
