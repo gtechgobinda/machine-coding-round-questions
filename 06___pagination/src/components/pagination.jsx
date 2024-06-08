f02416f51ab81b10d09c97471effe920aae3d334
Gobinda Saha <72671943+gtechgobinda@users.noreply.github.com>
2024-06-08 16:39:31 +0530

    some

--- 06___pagination/src/components/pagination.jsx
+++ 06___pagination/src/components/pagination.jsx
@@ -3,7 +3,6 @@ import React, { useEffect, useState } from "react";
 import "./pagination.css";
 const Pagination = () => {
   const [products, setProducts] = useState([]);
-  const [page, setPage] = useState(2);
 
   const fetchProducts = async () => {
     const res = await fetch("https://dummyjson.com/products?limit=100");
@@ -23,7 +22,7 @@ const Pagination = () => {
     <>
       {products.length > 0 && (
         <div className="products__all">
-          {products.slice(page * 10 - 10, page * 10).map((prod) => {
+          {products.map((prod) => {
             return (
               <>
                 <div className="products__single" key={prod.id}>
@@ -40,20 +39,7 @@ const Pagination = () => {
           })}
         </div>
       )}
-      {products.length > 0 && (
-        <div className="pagination">
-          <span>PREVIOUS</span>
-          {[...Array(products.length / 10)].map((_, i) => {
-            return (
-              <>
-                <span key={i}>{i + 1}</span>
-              </>
-            );
-          })}
-          {/* <span>1</span> */}
-          <span>NEXT</span>
-        </div>
-      )}
+
     </>
   );
 };
